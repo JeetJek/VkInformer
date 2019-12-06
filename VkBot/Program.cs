72cca48fdf38a8eeb83d9781970bd8744154bb06
Andrew <kaibazakov@yandex.ru>
2019-12-06 17:57:45 +0700

    Добавьте файлы проекта.

---
+++ VkBot/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using VkBotFramework;
+
+namespace VkInformer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VkBot infoBot;
+            if (File.Exists("Token"))
+                infoBot = new VkBot(File.ReadAllText("Token"), "https://vk.com/club189537036");
+            else
+                infoBot = new VkBot(Console.ReadLine(), Console.ReadLine());
+            infoBot.Api.Messages.Send(new VkNet.Model.RequestParams.MessagesSendParams
+            {
+                RandomId = Math.Abs(Environment.TickCount),
+                Message = "Test",
+                Domain = "jek_ouwl"
+            });
+
+            infoBot.Start();
+        }
+    }
+}
