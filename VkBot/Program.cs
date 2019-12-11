bad9d43a8bac4542237a69517d621c3f8f89ca6e
Andrew <kaibazakov@yandex.ru>
2019-12-11 10:38:51 +0700

    Добавил ключи запуска

--- VkBot/Program.cs
+++ VkBot/Program.cs
@@ -11,14 +11,66 @@ namespace VkInformer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Запуск");
             VkBot infoBot;
+            String Token="";
+            String Group="";
+            if(args.Length!=0)
+            {
+                if(args[0]== "-t")
+                {
+                    Console.WriteLine("-t использовано");
+                    try
+                    {
+                        Token=args[1];
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Не задан токен!");
+                    }
+                }
+                if(args[2]== "-g")
+                {
+                    Console.WriteLine("-g использовано");
+                    try
+                    {
+                        Group=args[3];
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Не задана группа!");
+                    }
+                }
+                if(args[0]== "-h")
+                {
+                    Console.WriteLine("-h  -  Вызов справки");
+                    Console.WriteLine("-t  -  Задать токен");
+                    Console.WriteLine("-g  -  Задать группу");
+                }
+            }
             if (File.Exists("Token"))
-                infoBot = new VkBot(File.ReadAllText("Token"), "https://vk.com/club189537036");
+            {
+                Console.WriteLine(File.ReadAllText("Token"));
+                infoBot = new VkBot(File.ReadAllText("Token").Replace("\n",""), "https://vk.com/club189537036");
+            }
             else
-                infoBot = new VkBot(Console.ReadLine(), Console.ReadLine());
+            {
+                if(Token!="" && Group!="")
+                {
+                    infoBot = new VkBot(Token, Group);
+                }
+                else
+                {
+                    Console.WriteLine("Введите токен и группу");
+                    infoBot = new VkBot(Console.ReadLine(), Console.ReadLine());
+                }
+            }
 
             String Msg="Температура за окном: "+ GetTemperature()+ "°C";
             Bot(ref infoBot, "jek_ouwl", Msg);
+            infoBot.Dispose();
+            Console.WriteLine("Закрытие");
+            return;
         }
         public static String GetTemperature()
         {
@@ -62,13 +114,7 @@ namespace VkInformer
                 Console.WriteLine(ex.ToString());
                 Console.WriteLine();
                 Console.WriteLine("################################");
-            }
-            finally
-            {
-                infoBot.Start();
-                infoBot.Dispose();
-            }
-            
+            }         
         }
     }
 }
